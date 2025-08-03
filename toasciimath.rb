615a1242edf132fb4293d81fcc568095e47e1d7f
Terence Liu <terence950831@gmail.com>
2025-08-03 14:17:30 +0800

    換行符號處理與優化程式

--- toasciimath.rb
+++ toasciimath.rb
@@ -1,32 +1,39 @@
 require 'plurimath'
 require 'cgi'
 
-#latex = '若$f\left(x\right)=\left\{\begin{array}{c} {x^{3} +1,\quad x\le 0}
-#         \\ {2+x,\quad 0<x<2} \\ {4,\quad x\ge 2} \end{array}\right.$，
-#         則 $f\left(f\left(0\right)\right)+f\left(f\left(3\right)\right)$=?'
 
-latex = '$f\left(x\right)=\frac{1}{2} \left(x+\left|x\right|\right)$；
-         $g\left(x\right)=\left\{\begin{array}{c} {1;\quad 1<x} \\ 
-         {2;\quad 0\le x\le 1} \\ {3;\quad x<0} \end{array}\right.$ ，'
-formula = Plurimath::Math.parse(latex, :latex)
-asciimath = formula.to_asciimath
+latex_list = [
+  '若$f\left(x\right)=\left\{\begin{array}{c} {x^{3} +1,\quad x\le 0} \\ {2+x,\quad 0<x<2} \\ {4,\quad x\ge 2} \end{array}\right.$，則 $f\left(f\left(0\right)\right)+f\left(f\left(3\right)\right)$=?',
+  '$f\\left(g\\left(x\\right)\\right)=\\left\\{\\begin{array}{c} {0;\\quad -1<x} \\\\ {1;\\quad -5\\le x\\le -1} \\\\ {3;\\quad x<-5} \\end{array}\\right.$',
+  '利用絕對值特性，得$f\left(x\right)= \frac{1}{2}(x+\left| x \right|)=\left\{\begin{array}{c} {x;\quad x\geqslant 0} \\ {0;\quad x<0} \end{array}\right.$ ，'
+]
 
-#把left right給移除使括號正常顯示
-asciimath.gsub!(/left/, "")
-asciimath.gsub!(/right/, "")
+latex_list.each do |latex|
+  formula = Plurimath::Math.parse(latex, :latex)
+  asciimath = formula.to_asciimath
 
+  # 移除 left/right 使括號正常顯示
+  asciimath.gsub!(/left/, "")
+  asciimath.gsub!(/right/, "")
 
-#把中文給還原
-asciimath = CGI.unescapeHTML(asciimath)
-asciimath = asciimath.gsub(/&#x([\da-fA-F]+);/) { [$1.hex].pack("U") }
+  # 還原中文
+  asciimath = CGI.unescapeHTML(asciimath)
+  asciimath = asciimath.gsub(/&#x([\da-fA-F]+);/) { [$1.hex].pack("U") }
 
-#讓換行符號正確顯示(這也太鬼畫符了吧)
-asciimath.gsub!(/:\[(.*?)\]:/m) do
-  body = $1.gsub(/"\s*"/, '),(')
-  ":(" + body + "):"
+  # 換行符號處理(也太鬼畫符了吧)
+  asciimath.gsub!(/:\[(.*?)\]:/m) do
+    body = $1.gsub(/"\s*"/, '],[')
+    ":[" + body + "]:"
+  end
+
+  #一些小調整
+  asciimath.gsub!(/"P{geqslant}"/,'>=')
+  asciimath.gsub!(/\$/,"")
+
+  puts asciimath
+  puts
 end
 
-puts asciimath
 
 
 
